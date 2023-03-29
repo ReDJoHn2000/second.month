c2e6fc27f207a20c34a38c31207a3dd8555b0180
git push -u origin <amirbek.pulatov@gmail.com>
2023-03-30 01:54:34 +0500

    added: Second Practical Lesson

---
+++ 07March2023-2-lesson-pr/main/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	// "strconv"
+)
+
+// func main() {
+// 	var odd, even int
+// 	var num string
+// 	fmt.Print("Input number: ")
+// 	fmt.Scan(&num)
+
+// 	for i := 0; i < len(num); i++ {
+		
+// 		if i % 2 == 1{
+// 			num_odd, _ := strconv.Atoi(string(num[i]))
+// 			odd += num_odd
+// 		} else if i % 2 == 0{
+// 			num_even, _ := strconv.Atoi(string(num[i]))
+// 			even += num_even
+// 		}
+// 	}
+
+// 	fmt.Println(odd - even)
+// }
